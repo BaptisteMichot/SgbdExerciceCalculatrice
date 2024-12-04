795429bcc13c38115b54210f65e94a73c82e7641
Baptiste <bmichot03@gmail.com>
2024-12-04 14:02:32 +0100

    ajout des boutons d'opérations

--- src/main/java/Calculatrice/App.java
+++ src/main/java/Calculatrice/App.java
@@ -3,6 +3,7 @@ package Calculatrice;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -20,19 +21,30 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        HBox hBox = new HBox(5);
-        VBox vBox = new VBox(5);
-
+        //Ecran de la calculatrice
         TextField affichage = new TextField();
-        affichage.setEditable(true);
+        affichage.setPrefWidth(415);
+        affichage.setPrefHeight(75);
+        affichage.setStyle("-fx-font-size: 35px;");
+        affichage.setAlignment(Pos.BASELINE_RIGHT);
+        affichage.textProperty().addListener((observable, ancienAffichage, nouvelAffichage) -> {
+            //0 ou plusieurs chiffres suivis de 0 ou 1 point suivi de 0 ou plusieurs chiffres
+            if (!nouvelAffichage.matches("\\d*\\.?\\d*")) { 
+                affichage.setText(ancienAffichage);
+            }
+        });
 
         //Boutons 0 à 9
-        Button[] boutonsChiffres = new Button[10];
+        Button[] chiffres = new Button[10];
         for(int i=0; i<=9; i++){
 
-            boutonsChiffres[i] = new Button(String.valueOf(i));
+            chiffres[i] = new Button(String.valueOf(i));
             int chiffre = i;
-            boutonsChiffres[i].setOnAction(new EventHandler<ActionEvent>(){
+            chiffres[i].setStyle("-fx-font-size: 35px;");
+            chiffres[i].setPrefWidth(100);
+            chiffres[i].setPrefHeight(100);
+
+            chiffres[i].setOnAction(new EventHandler<ActionEvent>(){
                 @Override
                 public void handle(ActionEvent event) {
                     affichage.appendText(String.valueOf(chiffre));
@@ -41,28 +53,78 @@ public class App extends Application {
         }
 
         //Boutons d'opérations
+        Button virgule = new Button(".");
+        virgule.setStyle("-fx-font-size: 35px;");
+        virgule.setPrefWidth(100);
+        virgule.setPrefHeight(100);
+
+        Button plus = new Button("+");
+        plus.setStyle("-fx-font-size: 35px;");
+        plus.setPrefWidth(100);
+        plus.setPrefHeight(100);
+
+        Button moins = new Button("-");
+        moins.setStyle("-fx-font-size: 35px;");
+        moins.setPrefWidth(100);
+        moins.setPrefHeight(100);
+
+        Button fois = new Button("*");
+        fois.setStyle("-fx-font-size: 35px;");
+        fois.setPrefWidth(100);
+        fois.setPrefHeight(100);
+
+        Button divise = new Button("/");
+        divise.setStyle("-fx-font-size: 35px;");
+        divise.setPrefWidth(100);
+        divise.setPrefHeight(100);
+
+        Button egal = new Button("=");
+        egal.setStyle("-fx-font-size: 35px;");
+        egal.setPrefWidth(205);
+        egal.setPrefHeight(100);
+
+        Button supprimer = new Button("←");
+        supprimer.setStyle("-fx-font-size: 35px;");
+        supprimer.setPrefWidth(100);
+        supprimer.setPrefHeight(100);
+
+        Button ac = new Button("AC");
+        ac.setStyle("-fx-font-size: 35px;");
+        ac.setPrefWidth(205);
+        ac.setPrefHeight(100);
         
+
         
         //Placement des boutons dans la grille
         GridPane grid = new GridPane();
         grid.setHgap(5);
         grid.setVgap(5);
-        grid.add(boutonsChiffres[7],0,0);
-        grid.add(boutonsChiffres[8],1,0);
-        grid.add(boutonsChiffres[9],2,0);
-        grid.add(boutonsChiffres[4],0,1);
-        grid.add(boutonsChiffres[5],1,1);
-        grid.add(boutonsChiffres[6],2,1);
-        grid.add(boutonsChiffres[1],0,2);
-        grid.add(boutonsChiffres[2],1,2);
-        grid.add(boutonsChiffres[3],2,2);
-        grid.add(boutonsChiffres[0],1,3);
+        grid.add(chiffres[7],0,0);
+        grid.add(chiffres[8],1,0);
+        grid.add(chiffres[9],2,0);
+        grid.add(chiffres[4],0,1);
+        grid.add(chiffres[5],1,1);
+        grid.add(chiffres[6],2,1);
+        grid.add(chiffres[1],0,2);
+        grid.add(chiffres[2],1,2);
+        grid.add(chiffres[3],2,2);
+        grid.add(chiffres[0],1,3);
+        grid.add(divise,3,0);
+        grid.add(fois,3,1);
+        grid.add(moins,3,2);
+        grid.add(plus,3,3);
+        grid.add(virgule,2,3);
+        grid.add(supprimer,0,3);
 
 
-
-        vBox.getChildren().addAll(affichage, grid);
-        hBox.getChildren().addAll(vBox);
-        scene = new Scene(hBox, 200, 300);
+        
+        HBox hBoxBas = new HBox(5, ac, egal);
+        VBox vBox = new VBox(5, affichage, grid, hBoxBas);
+        HBox hBox = new HBox(5, vBox);
+        vBox.setAlignment(Pos.CENTER);
+        hBox.setAlignment(Pos.CENTER);
+        
+        scene = new Scene(hBox, 500, 650);
         stage.setScene(scene);
         stage.show();
     }
